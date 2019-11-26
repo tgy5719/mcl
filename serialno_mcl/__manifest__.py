759321ce0ec32a21943d2e5cc77270e11936d138
adarsh <adarsh@prixgen.com>
2019-11-26 17:18:16 +0530

    sales person serial no

---
+++ serialno_mcl/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Post Date',
+    'version': '12.0.1.0.0',
+    'author': 'Ascents Entrepreneurs',
+    'license': 'OPL-1',
+    'category': 'Tools',
+    'summary': 'Set Effective Date on Stock Picking',
+    'description': """
+Set Effective Date on Stock Picking
+-----------------------------------
+
+Set Effective Date on Stock Picking
+""",
+    'depends': ['stock', 'base'],
+    'data': [
+        'views/sno_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'currency': 'EUR',
+    'price': 20,
+}
