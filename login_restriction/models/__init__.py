827b511cf8a28cc91138c25278c16b2063a4bb7e
adarsh <adarsh@prixgen.com>
2019-11-27 13:47:49 +0530

    login

--- login_restriction/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import crm_lead
-from . import crm_lead_login_restriction
-from . import res_users
--- login_restriction/models/crm_lead.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api, _,exceptions
-
-class CrmLead(models.Model):
-	_inherit = 'crm.lead'
-
-	z_pricelist_id = fields.Many2one('product.pricelist',string="Pricelist",related="user_id.property_product_pricelist")
-	z_sales_office = fields.Many2one('office.name',string="Sales Office",related="user_id.z_sales_office")
