3ec34ad34194058eb92d0fe430c4f92074e06cc0
adarsh <adarsh@prixgen.com>
2019-11-27 14:53:37 +0530

    account report inherhit

---
+++ account_report_inherit/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name' : 'Accounting Reports for sales office',
+    'summary': 'View and create reports',
+    'version': '12.0.0.2',
+    'category': 'Accounting',
+    'author': 'Prixgen Tech Solutions',
+    'website': 'https://www.prixgen.com',
+    'description': """
+Accounting Reports
+==================
+    """,
+    'depends': ['account_accountant','account_reports','addon_fields_for_mcl'],
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'auto_install': True,
+    'installable': True,
+    'license': 'OEEL-1',
+}
---
+++ account_report_inherit/model/account_aged_partner.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, api, fields, _
+from odoo.tools.misc import format_date
+
+
+class report_account_aged_partner(models.AbstractModel):
+    _inherit = "account.aged.partner"
+
+    filter_date = {'date': '', 'filter': 'today'}
+    filter_unfold_all = False
+    filter_partner = True
+
+    def _get_columns_name(self, options):
+        columns = [{}]
+        columns += [
+            {'name': v, 'class': 'number', 'style': 'white-space:nowrap;'}
+            for v in [_("JRNL"), _("Account"), _("Reference"), _("Sales Office"), _("Sales Team"),_("Salesperson"), _("Not due on: %s") % format_date(self.env, options['date']['date']),
+                      _("1 - 30"), _("31 - 60"), _("61 - 90"), _("91 - 120"), _("Older"), _("Total")]
+        ]
+        return columns
+
+    def _get_templates(self):
+        templates = super(report_account_aged_partner, self)._get_templates()
+        templates['main_template'] = 'account_reports.template_aged_partner_balance_report'
+        try:
+            self.env['ir.ui.view'].get_view_id('account_reports.template_aged_partner_balance_line_report')
+            templates['line_template'] = 'account_reports.template_aged_partner_balance_line_report'
+        except ValueError:
+            pass
+        return templates
+
+    @api.model
+    def _get_lines(self, options, line_id=None):
+        sign = -1.0 if self.env.context.get('aged_balance') else 1.0
+        lines = []
+        account_types = [self.env.context.get('account_type')]
+        results, total, amls = self.env['report.account.report_agedpartnerbalance'].with_context(include_nullified_amount=True)._get_partner_move_lines(account_types, self._context['date_to'], 'posted', 30)
+        for values in results:
+            if line_id and 'partner_%s' % (values['partner_id'],) != line_id:
+                continue
+            vals = {
+                'id': 'partner_%s' % (values['partner_id'],),
+                'name': values['name'],
+                'level': 2,
+                'columns': [{'name': ''}] * 6 + [{'name': self.format_value(sign * v)} for v in [values['direction'], values['4'],
+                                                                                                 values['3'], values['2'],
+                                                                                                 values['1'], values['0'], values['total']]],
+                'trust': values['trust'],
+                'unfoldable': True,
+                'unfolded': 'partner_%s' % (values['partner_id'],) in options.get('unfolded_lines'),
+            }
+            lines.append(vals)
+            if 'partner_%s' % (values['partner_id'],) in options.get('unfolded_lines'):
+                for line in amls[values['partner_id']]:
+                    aml = line['line']
+                    caret_type = 'account.move'
+                    if aml.invoice_id:
+                        caret_type = 'account.invoice.in' if aml.invoice_id.type in ('in_refund', 'in_invoice') else 'account.invoice.out'
+                    elif aml.payment_id:
+                        caret_type = 'account.payment'
+                    vals = {
+                        'id': aml.id,
+                        'name': aml.date_maturity or aml.date,
+                        'class': 'date',
+                        'caret_options': caret_type,
+                        'level': 4,
+                        'parent_id': 'partner_%s' % (values['partner_id'],),
+                        'columns': [{'name': v} for v in [aml.journal_id.code, aml.account_id.code, self._format_aml_name(aml),aml.invoice_id.z_sale_ofc.name,aml.invoice_id.team_id.name,aml.invoice_id.user_id.name]] +\
+                                   [{'name': v} for v in [line['period'] == 6-i and self.format_value(sign * line['amount']) or '' for i in range(7)]],
+                        'action_context': aml.get_action_context(),
+                    }
+                    lines.append(vals)
+        if total and not line_id:
+            total_line = {
+                'id': 0,
+                'name': _('Total'),
+                'class': 'total',
+                'level': 2,
+                'columns': [{'name': ''}] * 6 + [{'name': self.format_value(sign * v)} for v in [total[6], total[4], total[3], total[2], total[1], total[0], total[5]]],
+            }
+            lines.append(total_line)
+        return lines
+
+
+
